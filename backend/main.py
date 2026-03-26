230ac0d0bf66e20265f04854a7f627ac118c55b4
Shreyas K <kshreyas12345@gmail.com>
2026-03-26 18:19:20 +0530

    fix CORS for production frontend

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # CORS — allow Next.js frontend (adjust origin for production)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://graph-query-systemm.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
